9cb80db08714b58623cc45c4ef0b72b702bee13a
Jatin Chaudhary <09jatin2005@gmail.com>
2024-05-27 12:04:32 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subscriptionScheme = mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId, //one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const subscription = mongoose.model("subscription", subscriptionScheme);
